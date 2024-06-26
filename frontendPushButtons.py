80cdf6a1e6e8c00a2f7b07f1a03f564ae24bbab2
ingr1mm <carsten.staerkert@gmail.com>
2024-06-26 17:22:05 +0200

    ende tag1

--- frontendPushButtons.py
+++ frontendPushButtons.py
@@ -7,18 +7,18 @@ class pushButton:
     def __init__(self):
         self.widgetSize=QWidget.size(100, 100)
 
-class pushButton1(pushButton):
+class pushButton1(pushButton, backend.namePortal1, backend.linkPortal1):
     def __init__(self):
         self.widgetType=QPushButton()
-        self.widgetName=QWidget.setObjectName("pushButton1")
+        self.widgetName=QWidget.setObjectName(backend.namePortal1)
 
     # beim Klick auf den Button wird die Internetadresse der Variablen linkPortal1 aus backend.py aufgerufen
-    def executeLink(backend.linkPortal1):
+    def executeLink(self):
         webbrowser.open(backend.linkPortal1)        
         
-class pushButton2(pushButton):
+class pushButton2(pushButton, backend.namePortal2, backend.linkPortal2):
     def __init__(self):
         self.widgetType=QPushButton()
-        self.widgetName=QWidget.setObjectName("pushButton2")
+        self.widgetName=QWidget.setObjectName(backend.namePortal2)
         self.widgetNumber=2
         self.widgetLink=backend.linkPortal2
